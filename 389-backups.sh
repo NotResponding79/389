079f5d92d6646410ebe3cfd0d602efad30bf2ea5
NotResponding79 <50028405+NotResponding79@users.noreply.github.com>
2019-06-07 12:12:42 -0500

    Update 389-backups.sh

--- 389-backups.sh
+++ 389-backups.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-################################################################################                                                                                             ######
-################################################################################                                                                                             ######
+######################################################################################
+######################################################################################
 # NAME: 389-backup.sh
-# PURPOSE:This script backups the 389 Directory Server Database and takes and LD                                                                                             IF
+# PURPOSE:This script backups the 389 Directory Server Database and takes and LDIF
 # export of the DS.
 # It stores these backups in /var/dirsrv/backups/ldap.<TIMESTAMP>/
-# When run, it removes directories whose TIMESTAMP is greater than the ARCHIVE_P                                                                                             ERIOD set.
-################################################################################                                                                                             ########
-################################################################################                                                                                             #########
+# When run, it removes directories whose TIMESTAMP is greater than the ARCHIVE_PERIOD set.
+########################################################################################
+#########################################################################################
 
 
 PATH='/bin:/usr/bin:/sbin:/usr/sbin'
@@ -36,28 +36,28 @@ BINDDN='cn=directory manager'
 #bak2db /dir/to/backup
 
 # Create a directory for the LDAP backup.
-[ ! -d "${BACKUP_DIR}/ldap.${TIMESTAMP}" ] && ${ECHO} mkdir -m 0750 -p ${BACKUP_                                                                                             DIR}/ldap.${TIMESTAMP}
+[ ! -d "${BACKUP_DIR}/ldap.${TIMESTAMP}" ] && ${ECHO} mkdir -m 0750 -p ${BACKUP_DIR}/ldap.${TIMESTAMP}
 ${ECHO} chmod -R 750 ${BACKUP_DIR}
 ${ECHO} chown -R nobody:nobody ${BACKUP_DIR}
 
 # Extract an LDIF from the current LDAP configuration.
-${ECHO} /usr/sbin/db2ldif -Z ${HOST} -n userRoot -a ${BACKUP_DIR}/ldap.${TIMESTA                                                                                             MP}/userRoot.ldif -U
+${ECHO} /usr/sbin/db2ldif -Z ${HOST} -n userRoot -a ${BACKUP_DIR}/ldap.${TIMESTAMP}/userRoot.ldif -U
 
 # Create a backup of the LDAP database.
-${ECHO} /usr/sbin/db2bak.pl -Z ${HOST} -A ${BACKUP_DIR}/ldap.${TIMESTAMP}/db -D                                                                                              "${BINDDN}" -j /root/.ldapj -P STARTTLS
+${ECHO} /usr/sbin/db2bak.pl -Z ${HOST} -A ${BACKUP_DIR}/ldap.${TIMESTAMP}/db -D "${BINDDN}" -j /root/.ldapj -P STARTTLS
 
 # Clean /remove backups older than $ARCHIVE_PERIOD days.
 ARCHIVE=$(( ${ARCHIVE_PERIOD} - 1))
 
 ${ECHO} sleep 60
 
-#DIRECTORIES=$(find ${BACKUP_DIR} -type d -name ldap.* -mtime +${ARCHIVE_PERIOD}                                                                                              )
+#DIRECTORIES=$(find ${BACKUP_DIR} -type d -name ldap.* -mtime +${ARCHIVE_PERIOD} )
 
-for DIR in $(find ${BACKUP_DIR} -type d -name "*ldap.*" -mtime +${ARCHIVE_PERIOD                                                                                             } ); do
+for DIR in $(find ${BACKUP_DIR} -type d -name "*ldap.*" -mtime +${ARCHIVE_PERIOD} ); do
   ${ECHO} rm -rf ${DIR}
 done
 
-for DIR in $(find /var/lib/dirsrv/slapd-${HOST}/bak -type d -name "${HOST}-*" -m                                                                                             time +${ARCHIVE_PERIOD} ); do
+for DIR in $(find /var/lib/dirsrv/slapd-${HOST}/bak -type d -name "${HOST}-*" -mtime +${ARCHIVE_PERIOD} ); do
   ${ECHO} rm -rf ${DIR}
 done
 
